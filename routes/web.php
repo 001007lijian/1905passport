cd2d33133ff0b6baf673e73cef9719e5c0daa22d
lijian <lijian@localhost.localdomain>
2020-01-11 16:08:54 +0800

    passsport

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::post('/api/user/reg','UserController@reg');     //注册
+Route::post('/api/user/login','UserController@login'); // 登录
+Route::get('/api/show/time','UserController@showTime'); // 获取数据
